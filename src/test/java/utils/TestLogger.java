0d3fe661a94057dbab9e63113175844eaa994f7c
ajjankowski <aj.jankowski92@gmail.com>
2023-08-26 12:39:38 +0200

    TestLogger added

---
+++ src/test/java/utils/TestLogger.java
@@ -0,0 +1,8 @@
+package utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestLogger {
+    public static final Logger log = LoggerFactory.getLogger(TestLogger.class);
+}
