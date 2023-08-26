e6a574c2604e0ce3773ce21643341fd72df5bad1
ajjankowski <aj.jankowski92@gmail.com>
2023-08-26 12:39:17 +0200

    DriverFactory added

---
+++ src/test/java/utils/DriverFactory.java
@@ -0,0 +1,57 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.time.Duration;
+
+import static utils.TestLogger.log;
+
+public class DriverFactory {
+
+    public static WebDriver driver;
+
+    public static void setDriver(String driverType) {
+        switch (driverType.toLowerCase().trim()) {
+            case "chrome" -> {
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+//                chromeOptions.addArguments("--headless=new");
+//                chromeOptions.addArguments("--remote-allow-origins=*");
+                driver = new ChromeDriver(chromeOptions);
+                driver.manage().window().setSize(new Dimension(1920, 1080));
+            }
+            case "firefox" -> {
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.addArguments("--headless");
+                driver = new FirefoxDriver(firefoxOptions);
+                driver.manage().window().setSize(new Dimension(1920, 1080));
+            }
+            case "edge" -> {
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--headless=new");
+                edgeOptions.addArguments("--remote-allow-origins=*");
+                driver = new EdgeDriver(edgeOptions);
+                driver.manage().window().setSize(new Dimension(1920, 1080));
+            }
+            default -> log.error("Wrong driver type: use Chrome, Firefox or Edge");
+        }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+    }
+
+    public static void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
