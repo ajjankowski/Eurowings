f483582423e402cb9669762fbb22c8f06c353f53
ajjankowski <aj.jankowski92@gmail.com>
2023-08-26 12:39:27 +0200

    CommonStepDefinition added

---
+++ src/test/java/steps/CommonStepDefinition.java
@@ -0,0 +1,36 @@
+package steps;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import io.cucumber.java.en.Given;
+import org.openqa.selenium.TakesScreenshot;
+import utils.DriverFactory;
+
+import static org.openqa.selenium.OutputType.BYTES;
+import static utils.DriverFactory.driver;
+import static utils.TestLogger.log;
+
+public class CommonStepDefinition {
+
+    @Given("I am using {string} browser")
+    public static void iAmUsingWebsite(String driverType) {
+        log.info("Starting tests using " + driverType + " driver");
+        DriverFactory.setDriver(driverType);
+    }
+
+    @After(order = 0)
+    public void takeScreenshotOnFailure(Scenario scenario) {
+        log.info("Taking screenshot on failure");
+        if (scenario.isFailed()) {
+            TakesScreenshot ts = (TakesScreenshot) driver;
+            byte[] src = ts.getScreenshotAs(BYTES);
+            scenario.attach(src, "image/png", "Screenshot_failed_Scenario_" + scenario);
+        }
+    }
+
+    @After(order = 1)
+    public static void closeScenario() {
+        log.info("Closing scenario");
+        DriverFactory.teardown();
+    }
+}
