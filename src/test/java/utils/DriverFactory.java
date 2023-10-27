a837e0037ac9c59512dcef832d0587661be254c7
ajjankowski <aj.jankowski92@gmail.com>
2023-10-27 13:29:47 +0200

    headless turned off

--- src/test/java/utils/DriverFactory.java
+++ src/test/java/utils/DriverFactory.java
@@ -30,20 +30,20 @@ public class DriverFactory {
             case "chrome" -> {
                 WebDriverManager.chromedriver().setup();
                 ChromeOptions chromeOptions = new ChromeOptions();
-                chromeOptions.addArguments("--headless=new");
+//                chromeOptions.addArguments("--headless=new");
                 chromeOptions.addArguments("--remote-allow-origins=*");
                 driver = new ChromeDriver(chromeOptions);
             }
             case "firefox" -> {
                 WebDriverManager.firefoxdriver().setup();
                 FirefoxOptions firefoxOptions = new FirefoxOptions();
-                firefoxOptions.addArguments("--headless");
+//                firefoxOptions.addArguments("--headless");
                 driver = new FirefoxDriver(firefoxOptions);
             }
             case "edge" -> {
                 WebDriverManager.edgedriver().setup();
                 EdgeOptions edgeOptions = new EdgeOptions();
-                edgeOptions.addArguments("--headless=new");
+//                edgeOptions.addArguments("--headless=new");
                 edgeOptions.addArguments("--remote-allow-origins=*");
                 driver = new EdgeDriver(edgeOptions);
             }
